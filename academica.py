63c39544bdebfc66bf140534e963258b51839851
Joe Esposito <joe@joeyespo.com>
2012-09-22 13:35:23 -0400

    Changed config order.

--- academica.py
+++ academica.py
@@ -12,6 +12,9 @@ __version__ = '0.1'
 # Flask application
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('default_config')
+# Configuration, using development when run locally, overridden by ENV
+if __name__ == '__main__':
+    app.config.from_pyfile('dev_config.py', silent=True)
 app.config.from_envvar('SETTINGS_MODULE', silent=True)
 
 
@@ -23,5 +26,4 @@ def index():
 
 # Run development server with the development settings
 if __name__ == '__main__':
-    app.config.from_pyfile('dev_config.py', silent=True)
     app.run(app.config['HOST'], app.config['PORT'], app.debug != False)
