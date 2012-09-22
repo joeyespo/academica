6336f687434e04d63fa42bd73f4aac9a79c12d16
Joe Esposito <joe@joeyespo.com>
2012-09-22 12:40:35 -0400

    Initial workable project.

---
+++ academica.py
@@ -0,0 +1,27 @@
+"""\
+Academica
+
+The academic site.
+"""
+
+from flask import Flask, render_template
+
+__version__ = '0.1'
+
+
+# Flask application
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('default_config')
+app.config.from_envvar('SETTINGS_MODULE', silent=True)
+
+
+#Views
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Run development server with the development settings
+if __name__ == '__main__':
+    app.config.from_pyfile('dev_config.py', silent=True)
+    app.run(app.config['HOST'], app.config['PORT'], app.debug != False)
